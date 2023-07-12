a760651998dcf537196f92d6d2027689de523fd4
Anita Mukherjee <100989758+Ani-05@users.noreply.github.com>
2023-07-12 20:25:11 +0530

    bmi

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var result = weight / (height * height);
+    res.send("Your BMI is " + result);
+});
+
+app.listen(3000, function () {
+    console.log("server running on port 3000")
+});
